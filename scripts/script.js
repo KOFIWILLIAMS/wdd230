7ffac158eb222edffcf4fc3f8488d68639202fdf
KOFIWILLIAMS <winnerwilliams1@gmail.com>
2023-01-08 13:26:58 -0800

    WEEK ONE

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@ function getyear(){
     let myDate = new Date();
     
     document.getElementById('current_year').textContent = myDate.getFullYear();
-    document.getElementById('last_updated').textContent = document.lastUpdated;
+    document.getElementById('last_updated').textContent = document.lastModified;
     }
