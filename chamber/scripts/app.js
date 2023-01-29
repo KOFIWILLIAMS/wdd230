d41d81e769917b814380ce3ea58552cc316ff1ee
KOFIWILLIAMS <winnerwilliams1@gmail.com>
2023-01-29 03:35:06 -0800

    week 4

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,3 +1,12 @@
+const todayDate = document.querySelector("#todayDate"); 
+
+const now = Date.now(); 
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+todayDate.innerHTML = `${fulldate}`;
+
+
+
 // FOR GET YEAR
 function getyear() {
     let myDate = new Date();
@@ -16,10 +25,72 @@ const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
 
-// // Full date
-// const headerdate = document.querySelector("#headerdate"); 
 
-// const now = Date.now(); 
-// const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
-// headerdate.innerHTML = `Today is ${fulldate}`;
+
+
+
+
+// // // Full date
+// // const headerdate = document.querySelector("#headerdate"); 
+
+// // const now = Date.now(); 
+// // const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+// // headerdate.innerHTML = `Today is ${fulldate}`;
+
+// const timeElement = document.querySelector(".time");
+// const dateElement = document.querySelector(".date");
+
+// /**
+//  * @param {Date} date
+//  */
+// function formatTime(date) {
+//   const hours12 = date.getHours() % 12 || 12;
+//   const minutes = date.getMinutes();
+//   const isAm = date.getHours() < 12;
+
+//   return `${hours12.toString().padStart(2, "0")}:${minutes
+//     .toString()
+//     .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+// }
+
+// /**
+//  * @param {Date} date
+//  */
+// function formatDate(date) {
+//   const DAYS = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday"
+//   ];
+//   const MONTHS = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December"
+//   ];
+
+//   return `${DAYS[date.getDay()]}, ${
+//     MONTHS[date.getMonth()]
+//   } ${date.getDate()} ${date.getFullYear()}`;
+// }
+
+// setInterval(() => {
+//   const now = new Date();
+
+//   timeElement.textContent = formatTime(now);
+//   dateElement.textContent = formatDate(now);
+// }, 200);
