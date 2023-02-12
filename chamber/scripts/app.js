f603e72af46087cabdeaa6c6729d62d16408c71b
KOFIWILLIAMS <winnerwilliams1@gmail.com>
2023-02-11 17:47:39 -0800

    week 6

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,3 +1,7 @@
+
+
+
+
 const todayDate = document.querySelector("#todayDate"); 
 
 const now = Date.now(); 
---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,14 @@
+let temperature = parseFloat(document.querySelector('#temperature').textContent);
+let windSpeed = parseFloat(document.querySelector('#windSpeed').textContent);
+
+
+if (temperature <= 50 && windSpeed > 3) {
+    let windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+    document.querySelector('#windChill').innerHTML = `Wind Chill: ${windChill.toFixed(2)}°F`;
+ 
+}
+else {
+    document.querySelector('#windChill').innerHTML = `Wind Chill: N/A`;
+}   
+
+
