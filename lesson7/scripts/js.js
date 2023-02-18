4f68da22ed194fa721503a07a1ea9651cc7f6a62
KOFIWILLIAMS <winnerwilliams1@gmail.com>
2023-02-18 04:52:47 -0800

    week 7

--- lesson7/scripts/js.js
+++ lesson7/scripts/js.js
@@ -1,27 +1,27 @@
 
-const imagesToLoad = document.querySelectorAll("img[image_src]");
 
+let imagesToLoad = document.querySelectorAll('img[image_src]');
 
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px",
-};
 
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("image_src"));
-    image.onload = () => { image.removeAttribute("image_src"); };
+  image.setAttribute('src', image.getAttribute('image_src'));
+  image.onload = () => {image.removeAttribute('image_src');};
 };
 
-if ("IntersectionObserver" in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-        });
-    }, imgOptions);
-
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
+  if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
     });
-}
-else {
-
-}
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
