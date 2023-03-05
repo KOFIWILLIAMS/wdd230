760a0eee3aad136332d4fae0bda334781a643682
KOFIWILLIAMS <winnerwilliams1@gmail.com>
2023-03-05 08:14:03 -0800

    week 9

--- chamber/scripts/fetchdata.js
+++ chamber/scripts/fetchdata.js
@@ -1,21 +1,3 @@
-const gridbutton = document.querySelector('#grid');
-const listbutton = document.querySelector('#list');
-const display = document.querySelector('div.grid')
-
-gridbutton.addEventListener('click', () => {
-    display.classList.add('grid');
-    display.classList.remove('list');
-})
-
-listbutton.addEventListener('click', showList);
-
-function showList() {
-    display.classList.add('list');
-    display.classList.remove('grid')
-}
-
-
-
 
 
 
@@ -85,6 +67,24 @@ const displayBusinessDetails = (businessDetails) => {
   } // end of forEach loop
   )
 } // end of function expression
+
+
+
+const gridbutton = document.querySelector('#grid');
+const listbutton = document.querySelector('#list');
+const display = document.querySelector('div.grid')
+
+gridbutton.addEventListener('click', () => {
+    display.classList.add('grid');
+    display.classList.remove('list');
+})
+
+listbutton.addEventListener('click', showList);
+
+function showList() {
+    display.classList.add('list');
+    display.classList.remove('grid')
+}
 
 
 
