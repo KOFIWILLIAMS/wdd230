9aca0c4f220c3c3836528cd9c42f5b96847e9f26
KOFIWILLIAMS <winnerwilliams1@gmail.com>
2023-03-09 12:17:54 -0800

    week 10

--- lesson10/script/weatherapi.js
+++ lesson10/script/weatherapi.js
@@ -17,7 +17,7 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       //console.log(data); // this is for testing the call
-      // displayResults(data);
+       displayResults(data);
     } else {
         throw Error(await response.text());
     }
